a6a621d414f4578e8ac87d1030a999fc0c2b7ed5
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-07-25 14:53:28 +0500

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -1,27 +1,154 @@
-questions = { // don't change this line
-        "1test": {
-            "question": "Тестовый вопрос",
-            "answers": [
-                "Да, это тестововый вопрос, тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест",
-                "Нет, я считаю иначе, должно быть так, и не так... тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест_тест, бла-бла-бла..."
-            ],
-            "results": {},
-            "nextquestion": {
-                "yes": "2test",
-                "no": "2test"
-            }
-        },
-        "2test": {
-            "question": "Тестовый вопрос2",
-            "answers": [
-                "Да",
-                "Нет"
-            ],
-            "results": {
-                "yes": "Auto",
-                "no": "Demo",
-            },
-            "nextquestion": {
-            }
-        }
+"use strict";
+
+var questions = [
+  {
+    question: "Тестовый вопрос",
+    answer: 1,
+    valuesYes: [
+      {
+        axis: "dir",
+        value: 3
+      }
+    ],
+    valuesNo: [
+      {
+        axis: "auto",
+        value: 3
+      }
+    ]
+  },
+  {
+    question: "Тестовый вопрос2",
+    answer: 0,
+    valuesYes: [
+      {
+        axis: "prog",
+        value: 3
+      }
+    ],
+    valuesNo: []
+  },    
+  {
+    question: "Тестовый вопрос3",
+    answer: 0,
+    valuesYes: [
+      {
+        axis: "mono",
+        value: 3
+      }
+    ],
+    valuesNo: [
+      {
+        axis: "poly",
+        value: 3
+      }
+    ]
+  }
+];
+
+var qn = 0; // Question number
+var prev_answer = null;
+
+function shuffle(array) {
+  var i = 0,
+    j = 0,
+    temp = null;
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+shuffle(questions);
+
+init_question();
+
+function init_question() {
+  document.getElementById("question-text").innerHTML = questions[qn].question;
+  document.getElementById(
+    "question-number"
+  ).innerHTML = "Вопрос %num% из %sum%"
+    .replace("%num%", qn + 1)
+    .replace("%sum%", questions.length);
+  if (qn == 0) {
+    document.getElementById("back_button").style.display = "none";
+    document.getElementById("back_button_off").style.display = "block";
+  } else {
+    document.getElementById("back_button").style.display = "block";
+    document.getElementById("back_button_off").style.display = "none";
+  }
+}
+
+function next_question(mult) {
+  questions[qn].answer = mult;
+  qn++;
+
+  if (qn < questions.length) {
+    init_question();
+  } else {
+    results();
+  }
+}
+function prev_question() {
+  if (qn == 0) {
+    return;
+  }
+  qn--;
+  init_question();
+}
+
+function calc_score(score, max_value) {
+  return ((100 * score) / max_value).toFixed(0);
+}
+
+function results() {
+  var axes = {};
+
+  for (var i = 0; i < questions.length; i++) {
+    var q = questions[i];
+
+    for (var j = 0; j < q.valuesYes.length; j++) {
+      var a = q.valuesYes[j];
+      if (!(a.axis in axes)) {
+        axes[a.axis] = {
+          val: 0,
+          sum: 0
+        };
+      }
+
+      if (q.answer > 0) {
+        axes[a.axis].val += q.answer * a.value;
+      }
+      axes[a.axis].sum += Math.max(a.value, 0);
     }
+
+    for (var j = 0; j < q.valuesNo.length; j++) {
+      var a = q.valuesNo[j];
+      if (!(a.axis in axes)) {
+        axes[a.axis] = {
+          val: 0,
+          sum: 0
+        };
+      }
+
+      if (q.answer < 0) {
+        axes[a.axis].val -= q.answer * a.value;
+      }
+      axes[a.axis].sum += Math.max(a.value, 0);
+    }
+  }
+
+  var url = "";
+  for (var aK in axes) {
+    if (axes[aK].val > 0) {
+      if (url != "") url += "&";
+      url += aK + "=" + calc_score(axes[aK].val, axes[aK].sum);
+    }
+  }
+  url = window.btoa(url);
+  url = "/results2/?" + url;
+
+  location.href = url;
+}
