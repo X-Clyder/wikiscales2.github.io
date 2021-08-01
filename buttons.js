d6e81dc80bab88e5006dfe67edfc1921e745728a
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-08-02 00:42:03 +0500

    Create buttons.js

---
+++ buttons.js
@@ -0,0 +1,17 @@
+buttons = {
+    "yes": {
+        <button onclick="next_question(1)" class="strong-agree">
+            Да
+        </button>
+    },
+    "no": {
+        <button onclick="next_question(1)" class="strong-disagree">
+            Нет
+        </button>
+    },
+    "Idk": {
+        <button onclick="next_question(1)" class="neutral">
+            Не знаю
+        </button>
+    }
+}
