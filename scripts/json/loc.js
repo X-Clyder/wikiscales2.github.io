a1637dfe73697228c8a74ee5f31ce567e219e111
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-08-02 21:16:38 +0500

    Create loc.js

---
+++ scripts/json/loc.js
@@ -0,0 +1,14 @@
+localisation = {
+    "en": {
+        "name": "English",
+        "title": "WikiScales2",
+        // Index
+        "index_startbutton": "Кликни чтобы начать!",
+        "index_text": "<h2>What is WikiScales2?</h2><p>WikiScales2 is, in essence, a wiki-quiz that attempts to assign an ideology that matches your views the closest by asking questions to you based on a flowchart model. This test will sort you into one of 57 different ideologies based on your answers to these questions; maybe you'll discover something you didn't know about yourself!</p><h2>I don't understand one of the questions!</h2><p>If there's anything you don't understand, we've tried to make any terminology easily looked up, and if it's still not clear, tell us so we can make some changes to the questions!</p><h2>I don't like my matched ideology!</h2><p>¯\\_(ツ)_/¯</p>",
+        // Instructions
+        "instructions_title": "Instructions",
+        "instructions_text": "You will be presented with a series of questions. For each one, click the button with your opinion on it.",
+        "instructions_gotitbutton": "Got it!",
+        "instructions_nevermind": "Wait, nevermind!"
+    }
+}
