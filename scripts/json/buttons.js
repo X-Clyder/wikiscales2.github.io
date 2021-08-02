aa6b379c7e2afe9693f0ab3e326706c42c733581
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-08-02 21:10:21 +0500

    Create buttons.js

---
+++ scripts/json/buttons.js
@@ -0,0 +1,18 @@
+buttons = { // don't change this line
+    "yes": {
+        "text_en": "Да",
+        "color": "#1b5e20"
+    },
+    "no": {
+        "text_en": "Нет",
+        "color": "#b71c1c"
+    },
+    "idk": {
+        "text_en": "Я не согласен со всеми вышеописанными мнениями",
+        "color": "#000000"
+    },
+    "apathy": {
+        "text_en": "Меня не интересует этот вопрос",
+        "color": "#cd0000"
+    }
+}
