d18d73eedf8527025a0a1d9fe3cb11801764f7d5
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-08-02 21:12:53 +0500

    Create questions.js

---
+++ scripts/json/questions.js
@@ -0,0 +1,40 @@
+questions = { // don't change this line
+        "test1": {
+            "question_en": "123123123",
+            "answers": [
+                "yes",
+                "no"
+            ],
+            "results": {},
+            "nextquestion": {
+                "yes": "test2",
+                "no": "test2"
+            }
+        },
+        "test2": {
+            "question_en": "456456456?",
+            "answers": [
+                "yes",
+                "no"
+            ],
+            "results": {
+                "yes": "autocracy",
+            },
+            "nextquestion": {
+                "no": "test3",
+            }
+        },
+        "test3": {
+            "question_en": "TESTTEST1112121BLABLAL--BL212121121212TESTTEST1112121BLABLAL--BL212121121212?",
+            "answers": [
+                "yes",
+                "no"
+            ],
+            "results": {
+                "yes": "democracy1",
+                "no": "democracy2",
+            },
+            "nextquestion": {
+            }
+        }
+    }
