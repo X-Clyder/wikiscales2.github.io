a208e6235109f6eadb3951acecb2f4f5418ffe58
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-08-07 23:09:35 +0500

    Update quiz2.js

--- quiz2.js
+++ quiz2.js
@@ -1,34 +1,43 @@
-questions = {
+questions = { // don't change this line
         "test1": {
-            "question": "123",
+            "question_en": "First",
             "answers": [
                 "yes",
-                "no"
+                "no",
+                "idk",
+                "apathy"
             ],
             "results": {},
             "nextquestion": {
                 "yes": "test2",
-                "no": "test2"
+                "no": "test2",
+                "idk": "test2",
+                "apathy": "test2"
             }
         },
         "test2": {
-            "question": "456",
+            "question_en": "456456456?",
             "answers": [
                 "yes",
                 "no"
             ],
-            "results": {},
+            "results": {
+                "yes": "autocracy",
+            },
             "nextquestion": {
                 "no": "test3",
             }
         },
         "test3": {
-            "question": "789",
+            "question_en": "TESTTEST1112121BLABLAL--BL212121121212TESTTEST1112121BLABLAL--BL212121121212?",
             "answers": [
                 "yes",
                 "no"
             ],
-            "results": {},
+            "results": {
+                "yes": "democracy1",
+                "no": "democracy2",
+            },
             "nextquestion": {
             }
         }
