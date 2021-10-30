88dc182b7498a78d983280de853eac532f68de6d
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-10-30 10:03:49 +0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,44 +1,6 @@
 "use strict";
-var questions;
 
-/*questions = [
-  {
-    "question": "Первый естовый вопрос.",
-    "answers": {
-      "text1": "Да",
-      "text2": false,
-      "text3": "Я хз",
-      "text4": false,
-      "text5": "Канеш ноу ноу ноу",
-      "weight": [1, 0, 0, 0, -1]
-    },
-    "effect": {
-      "meta": 10,
-      "stat": 0,
-      "auth": 0,
-      "part": 0
-    }
-  },
-  {
-    "question": "Второй тест222.",
-    "answers": {
-      "text1": "Да",
-      "text2": false,
-      "text3": "Я хз",
-      "text4": false,
-      "text5": "Канеш ноу ноу ноу",
-      "weight": [1, 0, 0, 0, -1]
-    },
-    "effect": {
-      "ptnl": 10,
-      "stat": 0,
-      "auth": 0,
-      "part": 0
-    }
-  }
-];*/
-
-/*var questions = [
+var questions = [
   {
     question: "Чем больше прав и возможностей у каждого участника, тем общество справедливей и честней.",
     answer: 0,
@@ -56,7 +18,7 @@ var questions;
     ]
   },
   {
-    question: "Любая власть должна быть оспоримой.",
+    question: "Любая власть должна быть оспоримой, каждый участник должен иметь право написать пост с конструктивной критикой, если ему не нравятся действия определённого члена администрации.",
     answer: 0,
     valuesYes: [
       {
@@ -67,121 +29,14 @@ var questions;
     valuesNo: [
       {
         axis: "c1",
-        value: 3
-      }
-    ]
-  },
-  {
-    question: "Проводить различные реформы нужно только при их одобрении их народом (большим количеством участников), посредством проведения голосований/референдумов; каждый участник должен иметь право голоса и право свободно участвовать в таком голосовании/референдуме.",
-    answer: 0,
-    valuesYes: [
-      {
-        axis: "c0",
-        value: 3
-      }
-    ],
-    valuesNo: [
-      {
-        axis: "c1",
-        value: 3
-      }
-    ]
-  },
-  {
-    question: "Назначать новых руководителей википроекта лучше всего посредством проведения соответствующих голосований на вики, вместо того чтобы оставлять это решение на усмотрение действующего(-их) бюрократа(-ов).",
-    answer: 0,
-    valuesYes: [
-      {
-        axis: "c0",
-        value: 3
-      }
-    ],
-    valuesNo: [
-      {
-        axis: "c1",
-        value: 3
-      }
-    ]
-  },
-  {
-    question: "Все некачественные, короткие, либо плохо оформленные статьи следует непременно подвергать удалению (НЕ согласен — все подобные статьи следует улучшать и дорабатывать, вместо того чтобы удалять; к удалению любых статей следует прибегать как можно реже).",
-    answer: 0,
-    valuesYes: [
-      {
-        axis: "delete",
-        value: 3
-      }
-    ],
-    valuesNo: [
-      {
-        axis: "inc",
-        value: 3
-      }
-    ]
-  },
-  {
-    question: "В процессе работы на вики, я стремлюсь к созданию полностью готовой и оформленной статьи всего одной большой правкой (НЕ согласен — я предпочитаю править статьи постепенно, внося мелкие правки раз за разом).",
-    answer: 0,
-    valuesYes: [
-      {
-        axis: "mono",
-        value: 3
-      }
-    ],
-    valuesNo: [
-      {
-        axis: "poly",
         value: 3
       }
     ]
   }
-];*/
+];
 
 var qn = 0; // Question number
 var prev_answer = null;
-
-ini_question = function() {
-  document.getElementById("question-text").innerHTML = questions[qn].question; //Sets question text
-  document.getElementById("question-number").innerHTML = "Question " + (qn + 1) + " of " + questions.length; // Sets question number
-  //Disables back button if answers is empty
-  if (answers.length === 0) {
-    document.getElementById("back_button").style.display = "none";
-    document.getElementById("back_button_off").style.display = "block";
-  } else {
-    document.getElementById("back_button").style.display = "block";
-    document.getElementById("back_button_off").style.display = "none";
-  }
-  if (questions[qn].answers.text1 !== false) {
-    document.getElementById("button1").style.display = "block";
-    document.getElementById("button1").innerHTML = questions[qn].answers.text1;
-  } else {
-    document.getElementById("button1").style.display = "none";
-  }
-  if (questions[qn].answers.text2 !== false) {
-    document.getElementById("button2").style.display = "block";
-    document.getElementById("button2").innerHTML = questions[qn].answers.text2;
-  } else {
-    document.getElementById("button2").style.display = "none";
-  }
-  if (questions[qn].answers.text3 !== false) {
-    document.getElementById("button3").style.display = "block";
-    document.getElementById("button3").innerHTML = questions[qn].answers.text3;
-  } else {
-    document.getElementById("button3").style.display = "none";
-  }
-  if (questions[qn].answers.text4 !== false) {
-    document.getElementById("button4").style.display = "block";
-    document.getElementById("button4").innerHTML = questions[qn].answers.text4;
-  } else {
-    document.getElementById("button4").style.display = "none";
-  }
-  if (questions[qn].answers.text5 !== false) {
-    document.getElementById("button5").style.display = "block";
-    return document.getElementById("button5").innerHTML = questions[qn].answers.text5;
-  } else {
-    return document.getElementById("button5").style.display = "none";
-  }
-};
 
 function shuffle(array) {
   var i = 0,
