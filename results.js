9611932c06dbd86a957621b715c3be671074ebd9
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-11-02 21:10:06 +0500

    Update results.js

--- results.js
+++ results.js
@@ -282,7 +282,7 @@ function onImageLoaded() {
         }
       }
       
-      var yPos = 95;
+      var yPos = 92;
       for (var b in bonus) {
         value = getQueryVariable(b);
         if (value > bonus[b]) {
