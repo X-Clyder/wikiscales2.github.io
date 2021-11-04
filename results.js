2ad3e0dc3ba8df4113a1ad9b76e6e381955e94ee
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-11-04 15:20:48 +0500

    Update results.js

--- results.js
+++ results.js
@@ -418,7 +418,7 @@ function onImageLoaded() {
         ctx.fillStyle = "#000000";
         ctx.font = "20px sans-serif";
         ctx.textAlign = "left";
-        ctx.fillText(axesDrawInfo[i]["name0"], axeMargin - 30, yPos + 25);
+        ctx.fillText(axesDrawInfo[i]["name0"], axeMargin - 25, yPos + 20);
 
         ctx.textAlign = "right";
         ctx.fillText(
@@ -427,7 +427,7 @@ function onImageLoaded() {
           yPos - 6
         );
 
-        yPos += 40;
+        yPos += 35;
       }
 
       var xShift = 0;
