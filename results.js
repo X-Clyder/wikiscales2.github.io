4677c0477942ca685a6c7b94ac4d83dbbd68c7dd
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2021-11-04 19:27:13 +0500

    Update results.js

--- results.js
+++ results.js
@@ -405,16 +405,9 @@ function onImageLoaded() {
     yPos += 20;
     //Text2
         ctx.fillStyle = "#000000";
-        ctx.font = "20px sans-serif";
-        ctx.textAlign = "right";
+        ctx.font = "30px sans-serif";
+        ctx.textAlign = "left";
         ctx.fillText(["Свобода vs. Цензура"], axeMargin - 10, yPos + 21);
-
-        /*ctx.textAlign = "right";
-        ctx.fillText(
-          ["name1"],
-          rPreview.width - axeMargin - 8,
-          yPos - 6
-        );*/
 
         yPos += 34;
     //==================================
