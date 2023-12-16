89d4013a884c0a00fcf8729897f2368f4bc0f55d
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2023-12-16 23:49:02 +0500

    Update results.js

--- results.js
+++ results.js
@@ -529,7 +529,7 @@ function onImageLoaded() {
           }
           ctx.drawImage(
             images[b],
-            rPreview.width / 2 - ((imagesPerRow - 1) * 100) / 2 + xShift - 200,
+            rPreview.width / 2 - ((imagesPerRow - 1) * 200) / 2 + xShift - 200,
             yPos - 27 + yShift
           );
           xShift += 100;
