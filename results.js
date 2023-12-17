a69b3f363cd4b940f2ae2da0c53bb8a399d6bd5b
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2023-12-17 14:44:51 +0500

    Update results.js

--- results.js
+++ results.js
@@ -192,7 +192,7 @@ if (!bonusEnabled) {
 }
 
 var images = {
-  inc: inc == 0 ? "/images/1.png" : "/images/2.png",
+  inc: value == 0 ? "/images/1.png" : "/images/2.png",
   delete: "/images/2.png",
   even: "/images/3.png",
   imm: "/images/4.png",
