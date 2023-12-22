b0d865b35ab984a64d71212b38df3514b9fde636
WikiScales2 <87929224+WikiScales2@users.noreply.github.com>
2023-12-22 18:20:57 +0500

    Update results.js

--- results.js
+++ results.js
@@ -194,7 +194,7 @@ if (!bonusEnabled) {
 var images = {
   inc: "c0" == 1 ? "/images/1.png" : "c0" == 0.75 ? "/images/2.png" : "c0" == 0.5 ? "/images/3.png" : "c0" == 0.25 ? "/images/4.png" : "/images/default.png",
   delete: "b0" == 1 ? "/images/2.png" : "b0" == 0.75 ? "/images/3.png" : "b0" == 0.5 ? "/images/4.png" : "b0" == 0.25 ? "/images/5.png" : "/images/default.png",
-  even: axisValue("c") == 1 ? "/images/2.png" : axisValue("c") == 0.75 ? "/images/3.png" : axisValue("c") == 0.5 ? "/images/4.png" : axisValue("c") == 0.25 ? "/images/5.png" : "/images/default.png",
+  even: axis("c") == 1 ? "/images/2.png" : axis("c") == 0.75 ? "/images/3.png" : axis("c") == 0.5 ? "/images/4.png" : axis("c") == 0.25 ? "/images/5.png" : "/images/default.png",
   imm: "/images/4.png",
   merge: "/images/5.png",
   separa: "/images/6.png",
@@ -205,7 +205,7 @@ var images = {
   ref: "/images/11.png",
   react: "/images/12.png",
   revo: "/images/13.png",
-  expan: "/images/14.png",
+  expan: axisValue("c") == 1 ? "/images/2.png" : axisValue("c") == 0.75 ? "/images/3.png" : axisValue("c") == 0.5 ? "/images/4.png" : axisValue("c") == 0.25 ? "/images/5.png" : "/images/default.png",
 };
 
 var numImageLoaded = 0;
